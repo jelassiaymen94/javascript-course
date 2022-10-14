37b22bbf761d18a4c5ec73963025a44c4a355c77
Jelassi Aymen <nhedber1@students.solano.edu>
2022-10-14 10:12:16 +0100

    exercice 2 and 3 done

--- array-exercice-2/main.js
+++ array-exercice-2/main.js
@@ -1,5 +1,5 @@
 import { countries as countryList } from "./countries.js";
-import { webTechs } from "./web_techs.js";
+import { webTechs as webTech } from "./web_techs.js";
 
 let text =
 	"I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
@@ -11,9 +11,46 @@ const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
 
 for (let i = 0; i < shoppingCart.length; i++) {
 	if (shoppingCart[i] == "Meat" || "Sugar") {
-	} else {  
+	} else {
 		shoppingCart.unshift("Meat");
 		shoppingCart.push("Sugar");
 	}
 }
-console.log(shoppingCart);
+
+shoppingCart.push("Sugar");
+
+shoppingCart[shoppingCart.indexOf("Tea")] = "Green Tea";
+
+/* console.log(shoppingCart);
+ */
+let countryFounded = countryList.find((country) => country == "Ethiopia");
+countryFounded
+	? console.log(countryFounded.toUpperCase())
+	: countryList.push("Ethiopia");
+
+let SASS = webTech.find((tech) => tech == "Sass");
+SASS ? console.log("Sass is a CSS preprocess") : webTech.push("Sass");
+
+const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"];
+const backEnd = ["Node", "Express", "MongoDB"];
+const fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
+let arrayLength = ages.length;
+
+ages.sort();
+
+let min = ages[0];
+let max = ages[arrayLength - 1];
+console.log(`Min ${min}`);
+console.log(`Max ${max}`);
+
+let middle = arrayLength / 2;
+arrayLength % 2 == 0
+	? console.log(`Middle: ${ages[middle]}`)
+	: console.log(`Middles: ${ages[middle]} ${ages[middle + 1]}`);
+Math.trunc(ages.reduce((a, b) => a + b) / arrayLength);
+
+console.log(Math.abs(min - ages[middle]) == Math.abs(max - ages[middle]));
--- playground/script.js
+++ playground/script.js
@@ -3,4 +3,6 @@ const array1 = ["Volvo", "BMW", "Ford", "Mercedes"];
 console.log(typeof array1);
 
 console.log(typeof array1);
-document.getElementById("demo").innerHTML = array1;
+document.getElementById("demo").innerHTML = array1.toString();
+
+// create a function for accessing array elements.
